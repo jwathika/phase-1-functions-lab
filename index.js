884ac7ef100853c6db3bdb9f1b0eb47d9fc3352c
jwathika <wathika02@gmail.com>
2024-04-03 21:19:19 +0300

    passing

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  return pickupLocation - 42;
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  let hq = 42 * 264;
+  let npickupLocation = pickupLocation * 264;
+  return npickupLocation - hq;
+}
+function distanceTravelledInFeet(dist1, dist2) {
+  let nist1 = dist1 * 264;
+  let ndist2 = dist2 * 264;
+  return ndist2 - ndist1;
+}
